58ad69882589a1e63109518f3f35d890abcb2830
Tom <2865368535@qq.com>
2022-10-26 00:00:08 +0800

    实现LoginPage页面全屏

--- app/src/main/java/cn/itcast/zhishang/LoginPage.java
+++ app/src/main/java/cn/itcast/zhishang/LoginPage.java
@@ -3,6 +3,7 @@ package cn.itcast.zhishang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        int flag = WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        getWindow().setFlags(flag, flag);
     }
 }
