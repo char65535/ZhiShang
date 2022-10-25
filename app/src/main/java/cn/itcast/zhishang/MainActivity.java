9a6a2fabd84362887985822aeb045fa67b8187c5
Tom <2865368535@qq.com>
2022-10-25 22:03:05 +0800

    实现 btn_skipPage按钮倒计时跳转功能

--- app/src/main/java/cn/itcast/zhishang/MainActivity.java
+++ app/src/main/java/cn/itcast/zhishang/MainActivity.java
@@ -5,16 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView, version;
     Button btn_skipPage;
+    private int recLen = 5;
+    Timer timer = new Timer();
+    private Handler handler;
+    private Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +34,42 @@ public class MainActivity extends AppCompatActivity {
 //        实现页面全屏
         int flag = WindowManager.LayoutParams.FLAG_FULLSCREEN;
         getWindow().setFlags(flag, flag);
-
-
+        timer.schedule(task, 1000, 1000);
+        handler = new Handler();
+        handler.postDelayed(runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
         initView();
 //        更改textView处文字字体
         textView.setTypeface(Typeface.createFromAsset(this.getAssets(), "fonts/huakangw5.ttc"));
-
-        //跳转到登录页面
-        Intent intent = new Intent();
-        intent.setClass(this, LoginPage.class);
-
     }
 
     private void initView() {
         textView = findViewById(R.id.textView);
         version = findViewById(R.id.version);
         btn_skipPage = findViewById(R.id.btn_skipPage);
-
     }
+
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    recLen--;
+                    btn_skipPage.setText("跳过 " + recLen + "s");
+                    if (recLen < 0) {
+                        timer.cancel();
+                        btn_skipPage.setVisibility(View.GONE);
+                    }
+                }
+            });
+        }
+    };
+
 }
